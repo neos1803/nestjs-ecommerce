9237f4399dcdb0657a18c6d63c4a8aa81f575ffc
Nanda Purba <nandawardipurba@gmail.com>
2020-11-04 14:21:02 +0700

    Edit swagger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
-import { AppModule } from './application/app.module';
+import { AppModule } from 'src/application/app.module';
+import { ApiBlueprintDocs } from 'src/infrastructure/config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder()
-    .setTitle('Online Store')
-    .setVersion('1.0')
-    .build();
-  
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('docs', app, document)
+  const apiBlueprint = new ApiBlueprintDocs(app);
+  apiBlueprint.setupCommon();
+  apiBlueprint.setupV1();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  console.log(`App listening on http://localhost:${process.env.PORT}`);
 }
 bootstrap();
